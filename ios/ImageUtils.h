1c7edc06b65255909086f9b2113a0b2c130b38fa
giga123Z <125711680+giga123Z@users.noreply.github.com>
2023-09-30 07:51:29 +0700

    Update ImageUtils.h

--- ios/ImageUtils.h
+++ ios/ImageUtils.h
@@ -7,6 +7,7 @@
 @interface ImageUtils :NSObject
 + (UIImage*)imagePadLeft:(NSInteger) left withSource: (UIImage*)source;
 + (uint8_t *)imageToGreyImage:(UIImage *)image;
++ (uint8_t *)imageToGreyImageWithThreshold:(UIImage *)image threshold:(NSInteger) threshold;
 + (UIImage *)imageWithImage:(UIImage *)image scaledToFillSize:(CGSize)size;
 + (NSData*)bitmapToArray:(UIImage*) bmp;
 + (NSData *)eachLinePixToCmd:(unsigned char *)src nWidth:(NSInteger) nWidth nHeight:(NSInteger) nHeight nMode:(NSInteger) nMode;
